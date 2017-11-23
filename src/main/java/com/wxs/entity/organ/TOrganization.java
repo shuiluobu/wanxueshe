764d56b3816ec679ab26941b8ca4657a1cfb4efd
wangyiheifxue <1178879424@qq.com>
2017-11-23 17:22:09 +0800

    教育机构 管理 相关代码

--- src/main/java/com/wxs/entity/organ/TOrganization.java
+++ src/main/java/com/wxs/entity/organ/TOrganization.java
@@ -7,15 +7,17 @@ import java.util.Date;
 import com.baomidou.mybatisplus.annotations.TableId;
 import com.baomidou.mybatisplus.activerecord.Model;
 import com.baomidou.mybatisplus.annotations.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 
 /**
  * <p>
- * <p>
+ * <p>教育机构
  * </p>
  *
- * @author skyer
+ * @author wyh
  * @since 2017-09-21
  */
 @TableName("t_organization")
@@ -24,27 +26,25 @@ public class TOrganization extends Model<TOrganization> {
     private static final long serialVersionUID = 1L;
 
     @TableId(value = "id", type = IdType.AUTO)
-    private Long id;
-    private String organCode;
-    private String organName;
-    private String city;
-    private String cityarea;
-    private String linkman;
-    private String telePhone;
-    private String mobilePhone;
-    private String coordinate;
-    private Integer leval;
-    private String introduce;
-
-
-    private Date createTime; //创建时间
-    private Integer status;
-    private Long createUser;
-    private String logoImg;
-    /**
-     * 机构简介
-     */
-    private String organRemark;
+    private Long id;                //主键，自增
+    private Long userId;           //用户Id
+    private String organCode;       //机构编号
+    private String organName;       //机构名称
+    private String province;        //所在省
+    private String city;            //所在城市
+    private String cityarea;        //所在市的区
+    private String address;         //详细地址
+    private String linkman;         //机构联系人
+    private String telePhone;       //座机
+    private String mobilePhone;     //手机号
+    private String coordinate;      //坐标
+    private Integer leval;          //等级
+    private String organRemark;     //机构备注
+    private String introduce;       //机构介绍
+    private Date createTime;        //创建时间
+    private Integer status;         //状态 1:启用，0：禁止
+    private Long createUser;        //添加该 机构的 用户Id(userId)
+    private String logoImg;         //图标
 
 
     public Long getId() {
@@ -53,6 +53,14 @@ public class TOrganization extends Model<TOrganization> {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
     }
 
     public String getOrganCode() {
@@ -134,7 +142,8 @@ public class TOrganization extends Model<TOrganization> {
     public void setIntroduce(String introduce) {
         this.introduce = introduce;
     }
-
+    @DateTimeFormat(pattern ="yyyy-MM-dd  HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     public Date getCreateTime() {
         return createTime;
     }
@@ -173,6 +182,22 @@ public class TOrganization extends Model<TOrganization> {
 
     public void setOrganRemark(String organRemark) {
         this.organRemark = organRemark;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     @Override
