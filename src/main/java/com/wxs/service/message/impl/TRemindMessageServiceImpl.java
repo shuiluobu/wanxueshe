3c34027246e3ca80f746df614ee327cf5392d446
leejc213@126.com <leejc213@126.com>
2017-11-27 18:15:20 +0800

    消息提醒类

---
+++ src/main/java/com/wxs/service/message/impl/TRemindMessageServiceImpl.java
@@ -0,0 +1,21 @@
+package com.wxs.service.message.impl;
+
+
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.wxs.entity.message.TRemindMessage;
+import com.wxs.mapper.message.TRemindMessageMapper;
+import com.wxs.service.message.ITRemindMessageService;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author skyer
+ * @since 2017-11-27
+ */
+@Service
+public class TRemindMessageServiceImpl extends ServiceImpl<TRemindMessageMapper, TRemindMessage> implements ITRemindMessageService {
+	
+}
