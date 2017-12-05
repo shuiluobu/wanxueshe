652f5c3d72237d7d1b5d48021e2edfae4d80e90d
wangyiheifxue <1178879424@qq.com>
2017-12-05 17:35:18 +0800

    企业微信：待办页面

---
+++ src/main/java/com/wxs/service/organ/ITOrganAgendaService.java
@@ -0,0 +1,18 @@
+package com.wxs.service.organ;
+
+import com.baomidou.mybatisplus.service.IService;
+import com.wxs.entity.organ.TOrganAgenda;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by wyh on 2017/12/5.
+ */
+public interface ITOrganAgendaService extends IService<TOrganAgenda> {
+
+    //获取用户的待办
+    public List<TOrganAgenda> myOrganAgenda(Long userId,String startTime,String endTime);
+    //获取用户所属机构所有的待办
+    public List<TOrganAgenda> organAgenda(Long userId,String startTime,String endTime);
+}
